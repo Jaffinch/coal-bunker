5fccaebce2802ac82294f9038201d7f237429bf8
Mashbar <josephmatthewson@hotmail.com>
2022-02-05 16:36:13 +0000

    Tiled Homepage
    https://www.mapeditor.org/

---
+++ CoalBunker/Tiled/maps/BasicIsometricShapes.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="BasicIsometricShapes" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <grid orientation="isometric" width="64" height="64"/>
+ <image source="BasicIsometricShapes.png" width="360" height="360"/>
+</tileset>
