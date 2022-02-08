d40a14836489fac20462adc19871ef8added425f
Mashbar <josephmatthewson@hotmail.com>
2022-02-08 22:56:32 +0000

    8 way sprite
    sprite draft for 8 directions, hooked it up to an animator, used a blend tree to handle the switching, basic script to point player in the direction they're moving

--- CoalBunker/Assets/Player/PlayerController.cs
+++ CoalBunker/Assets/Player/PlayerController.cs
@@ -11,14 +11,20 @@ public class PlayerController : MonoBehaviour
     private float directionX;
     private float directionY;
 
+    //temps to be deleted
+    private float prevX;
+    private float prevY;
+
     Vector2 velocity;
 
     //physics scripts
     Controller2D controller;
+    Animator anim;
 
     private void Start()
     {
         controller = GetComponent<Controller2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -28,6 +34,22 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
+        if(directionX == 0 && directionY == 0)
+        {
+            anim.SetFloat("PosX", prevX);
+            anim.SetFloat("PosY", prevY);
+        }
+        else
+        {
+            anim.SetFloat("PosX", directionX);
+            anim.SetFloat("PosY", directionY);
+            prevX = directionX;
+            prevY = directionY;
+
+        }
+        
+
+
         //move player
         if ((directionX > 0 || directionX < 0) && (directionY > 0 || directionY < 0))
         {
