bbb23038f7d60bcc99d75f30f7f661a3282ab1ca
Jack <jackmatthewson@me.com>
2022-05-23 22:57:38 +0100

    Cleanup after PR

--- CoalBunker/Assets/Scripts/Input/Input Actions.cs
+++ CoalBunker/Assets/Scripts/Input/Input Actions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Input/Input Actions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Input Actions.inputactions'
 
 using System;
 using System.Collections;
